a0909f865359ccb55987752c23fd5b3c0fdfe78d
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-02-16 15:53:38 +0100

    update compile command

--- Compile_Command_LDBL.sh
+++ Compile_Command_LDBL.sh
@@ -1,2 +1,2 @@
-echo g++ -std=c++17 SSOCs.cpp -I ~/git/EiCOS/include/ -L ~/git/EiCOS/ -l eicos_LDBL -Wl,-rpath,~/git/EiCOS/ -Ofast -o SecOrdCone_VarConsOrder_Bisection_LDBL.exe
-g++ -std=c++17 SSOCs.cpp -I ~/git/EiCOS/include/ -L ~/git/EiCOS/ -l eicos_LDBL -Wl,-rpath,~/git/EiCOS/ -Ofast -o SecOrdCone_VarConsOrder_Bisection_LDBL.exe
+echo g++ -std=c++17 SSOCs.cpp -I ~/git/EiCOS/include/ -L ~/git/EiCOS/ -l eicos_LDBL -Wl,-rpath,~/git/EiCOS/ -Ofast -o SSOCs_LDBL.exe
+g++ -std=c++17 SSOCs.cpp -I ~/git/EiCOS/include/ -L ~/git/EiCOS/ -l eicos_LDBL -Wl,-rpath,~/git/EiCOS/ -Ofast -o SSOCs_LDBL.exe
